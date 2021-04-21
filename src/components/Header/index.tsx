b231d238fa17a5f41621e5ea2fc6451a5e1ba99a
Geraldo Luiz <geraldobl58@gmail.com>
2021-04-21 16:57:42 -0300

    feat: Responsive Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,8 @@
-import { Flex, useBreakpointValue} from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue} from '@chakra-ui/react'
+
+import { RiMenuLine } from 'react-icons/ri'
+
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext'
 
 import { Logo } from './Logo'
 import { NotificationNav } from './NotificationsNav'
@@ -6,6 +10,8 @@ import { Profile } from './Profile'
 import { SearchBox } from './SearchBox'
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -23,6 +29,17 @@ export function Header() {
       align="center"
     >
      
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation" 
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
